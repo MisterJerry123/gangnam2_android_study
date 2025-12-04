c041be8084c0a5783ba59285a4b76af8c1fa0646
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-05 00:25:05 +0900

    feat: 연습문제3 RatingButton, FilterButton 완성

---
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/component/FilterButton.kt
@@ -0,0 +1,97 @@
+package com.survivalcoding.gangnam2kiandroidstudy.presentation.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.survivalcoding.gangnam2kiandroidstudy.R
+import com.survivalcoding.gangnam2kiandroidstudy.ui.AppColors
+import com.survivalcoding.gangnam2kiandroidstudy.ui.AppTextStyles
+
+@Composable
+fun FilterButton(text: String, isSelected: Boolean) {
+    when (isSelected) {
+        true -> {
+            Box(
+                modifier = Modifier
+                    .size(width = 50.dp, height = 28.dp)
+                    .background(color = AppColors.primary100, shape = RoundedCornerShape(10.dp))
+                    .border(
+                        color = AppColors.primary100,
+                        width = 1.dp,
+                        shape = RoundedCornerShape(10.dp)
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                Row(
+                    modifier = Modifier,
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    Text(
+                        text = text,
+                        style = AppTextStyles.smallerTextBold.copy(color = AppColors.white)
+                    )
+
+                }
+            }
+
+        }
+
+        false -> {
+            Box(
+                modifier = Modifier
+                    .size(width = 50.dp, height = 28.dp)
+                    .background(color = AppColors.white, shape = RoundedCornerShape(10.dp))
+                    .border(
+                        color = AppColors.primary80,
+                        width = 1.dp,
+                        shape = RoundedCornerShape(10.dp)
+                    ),
+
+                contentAlignment = Alignment.Center
+            ) {
+                Row(
+                    modifier = Modifier,
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    Text(
+                        text = text,
+                        style = AppTextStyles.smallerTextBold.copy(color = AppColors.primary80)
+                    )
+
+                }
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun FilterButtonPreview() {
+    Column {
+
+        FilterButton("Text", true)
+        Spacer(modifier = Modifier.height(30.dp))
+        FilterButton("Text", false)
+    }
+}
