8e9ebcd874a0eafec24ad068d19af55e66a62f5e
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-16 23:59:12 +0900

    feat: 레시피를 찾지 못했을 때 예외 처리 추가

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/domain/use_case/GetRecipeDetailsUseCase.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/domain/use_case/GetRecipeDetailsUseCase.kt
@@ -10,14 +10,13 @@ class GetRecipeDetailsUseCase(
     private val savedRecipesRepository: SavedRecipesRepository,
     private val proceduresRepository: ProcedureRepository
 ) {
-
     suspend fun execute(id: Int): Pair<List<Ingredients>, List<Procedure>> {
 
+        val recipe = savedRecipesRepository.getSavedRecipes().find { it.id == id }
+            ?: throw NoSuchElementException("Recipe with id $id not found")
         return Pair(
-            savedRecipesRepository.getSavedRecipes().filter { it.id == id }[0].ingredients,
+            recipe.ingredients,
             proceduresRepository.getProcedureByRecipeId(id)
         )
-
-
     }
 }
