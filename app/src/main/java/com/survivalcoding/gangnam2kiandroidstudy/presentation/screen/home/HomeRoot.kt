256a055b7f18e9764c17cbdbfddec21cec044429
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-18 10:35:32 +0900

    chore: 불필요한 로그 및 임포트 제거

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/home/HomeRoot.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/home/HomeRoot.kt
@@ -1,6 +1,5 @@
 package com.survivalcoding.gangnam2kiandroidstudy.presentation.screen.home
 
-import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -8,14 +7,8 @@ import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun HomeRoot(viewModel: HomeViewModel = koinViewModel()) {
-
     val homeState by viewModel.state.collectAsState()
-    Log.d("HomeRoot", "homeState: ${homeState.selectedCategory}")
-
     HomeScreen(homeState) {
-        Log.d("HomeScreen", "it: $it")
         viewModel.onSelectedCategory(it)
     }
-
-
 }
