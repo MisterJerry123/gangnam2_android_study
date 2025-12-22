8f7a28f6da3807ed993cf2815dd16670f0362331
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-22 20:20:11 +0900

    fix: Scaffold의 paddingValues를 콘텐츠에 적용
    - SearchRecipesScreen을 Box로 감싸고 Scaffold에서 제공하는 paddingValues 적용

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/search_recipes/SearchRecipesRoot.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/search_recipes/SearchRecipesRoot.kt
@@ -1,6 +1,8 @@
 package com.survivalcoding.gangnam2kiandroidstudy.presentation.screen.search_recipes
 
 import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarHost
@@ -11,6 +13,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -27,37 +30,39 @@ fun SearchRecipesRoot(viewModel: SearchRecipesViewModel = koinViewModel()) {
                 }
             }
         }
-    ) {
-        SearchRecipesScreen(
-            state = searchRecipesState,
-            onAction = { action ->
-                when (action) {
-                    is SearchRecipesAction.ToggleBottomSheet -> viewModel.toggleBottomSheet()
-                    is SearchRecipesAction.ApplyFilterRecipes -> {
-                        Log.d("SearchRecipesRoot", "SearchRecipesRoot: 진입")
-                        viewModel.applyFilterRecipe(
-                            searchText = action.searchText,
-                            time = action.time,
-                            rate = action.rate,
-                            category = action.category
-                        )
-                    }
+    ) { paddingValues ->
+        Box(modifier = Modifier.padding(paddingValues)) {
+            SearchRecipesScreen(
+                state = searchRecipesState,
+                onAction = { action ->
+                    when (action) {
+                        is SearchRecipesAction.ToggleBottomSheet -> viewModel.toggleBottomSheet()
+                        is SearchRecipesAction.ApplyFilterRecipes -> {
+                            Log.d("SearchRecipesRoot", "SearchRecipesRoot: 진입")
+                            viewModel.applyFilterRecipe(
+                                searchText = action.searchText,
+                                time = action.time,
+                                rate = action.rate,
+                                category = action.category
+                            )
+                        }
 
-                    is SearchRecipesAction.CancelFilterRecipes -> {
-                        viewModel.dismissFilterRecipe()
-                    }
+                        is SearchRecipesAction.CancelFilterRecipes -> {
+                            viewModel.dismissFilterRecipe()
+                        }
 
-                    is SearchRecipesAction.ChangeSearch -> {
-                        viewModel.filterRecipes(
-                            searchText = action.searchText,
-                            time = action.time,
-                            rate = action.rate,
-                            category = action.category
-                        )
+                        is SearchRecipesAction.ChangeSearch -> {
+                            viewModel.filterRecipes(
+                                searchText = action.searchText,
+                                time = action.time,
+                                rate = action.rate,
+                                category = action.category
+                            )
+                        }
                     }
-                }
-            },
-        )
+                },
+            )
+        }
     }
 
     LaunchedEffect(viewModel.event) {
