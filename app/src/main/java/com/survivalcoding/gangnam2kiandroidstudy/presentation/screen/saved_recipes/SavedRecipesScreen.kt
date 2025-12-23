5ee82f5299bca9bb130d2212cd345c5a6bcafbbe
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-23 17:33:55 +0900

    chore: 불필요한 import 제거 (android.util.Log)

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/saved_recipes/SavedRecipesScreen.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/saved_recipes/SavedRecipesScreen.kt
@@ -1,6 +1,5 @@
 package com.survivalcoding.gangnam2kiandroidstudy.presentation.screen.saved_recipes
 
-import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
