49e33d5424e3d04f0ca32be1e1c4625f56dc7792
MisterJerry123 <misterjerry12345@gmail.com>
2026-01-10 23:10:31 +0900

    feat: RecipeListFragment 아이템 클릭 시 토스트 메시지 추가
    - 레시피 아이템 클릭 시 해당 아이템의 ID를 포함한 토스트 메시지를 출력하도록 변경되었습니다.

--- app/src/main/java/com/misterjerry/gangnam2kiandroidstudy/presentation/screen/saved_recipes/legacy/RecipeListFragment.kt
+++ app/src/main/java/com/misterjerry/gangnam2kiandroidstudy/presentation/screen/saved_recipes/legacy/RecipeListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.lifecycle.lifecycleScope
@@ -57,6 +58,12 @@ class RecipeListFragment : Fragment(), RecipeListAdapter.OnItemClickListener {
     }
 
     override fun onItemClicked(pos: Int) {
+        Toast.makeText(
+            requireContext(),
+            "RecipeItem 클릭됨. id:${viewModel.state.value.savedRecipesList[pos].id}",
+            Toast.LENGTH_SHORT
+        ).show()
+
         val fragment =
             SavedRecipeDetailFragment.newInstance(viewModel.state.value.savedRecipesList[pos].name)
         parentFragmentManager.commit {
